58a120c6c426005103dc64cf51e471018cb5f438
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2022-11-27 16:10:22 -0500

    should have done the logging in a separate branch, but got carried away

--- pip_plus/constants.py
+++ pip_plus/constants.py
@@ -10,6 +10,10 @@ COMPARISON_OPERATORS: List[str] = [
     ">",
     "<",
 ]  # https://pip.pypa.io/en/stable/topics/dependency-resolution/
+
 INSTALL: str = "install"
 UNINSTALL: str = "uninstall"
 REQUIREMENTS_TXT: str = "requirements.txt"
+DEV_REQUIREMENTS_TXT: str = "requirements.dev.txt"
+TEST_REQUIREMENTS_TXT: str = "test/requirements.txt"
+
--- pip_plus/entrypoint.py
+++ pip_plus/entrypoint.py
@@ -7,10 +7,13 @@ import sys
 from typing import List
 from pip_plus.pinned_package import PinnedPackage
 from pip_plus import utils
+from pip_plus.logger import PipPlusLogger
 from pip_plus.constants import (
     INSTALL,
     UNINSTALL,
     REQUIREMENTS_TXT,
+    DEV_REQUIREMENTS_TXT,
+    TEST_REQUIREMENTS_TXT,
 )
 
 
@@ -24,20 +27,48 @@ def main():
     :returns None:
     """
 
-    if len(sys.argv) < 3 or sys.argv[1] != INSTALL and sys.argv[1] != UNINSTALL:
+    log = PipPlusLogger.get_logger(__name__)
+
+    if (
+        len(sys.argv) < 3
+        or (sys.argv[1] != INSTALL and sys.argv[1] != UNINSTALL)
+        or "-r" in sys.argv
+        or "--requirement" in sys.argv
+    ):
+        log.info(
+            "User did not provide 'install', 'uninstall', '-r', or '--requirement' arguments. Running 'pip' normally."
+        )
         utils.run_user_pip_cmd(sys.argv[1:])
         sys.exit(0)
 
     pip_option: str = sys.argv[1]
     virtual_env: str = environ.get("VIRTUAL_ENV")
+
+    requirements_file: str = REQUIREMENTS_TXT
+
+    if "--test" in sys.argv:
+        requirements_file = TEST_REQUIREMENTS_TXT
+        sys.argv.remove("--test")
+        log.debug(f"'--test' argument provided by user.")
+    elif "--dev" in sys.argv:
+        requirements_file = DEV_REQUIREMENTS_TXT
+        sys.argv.remove("--dev")
+        log.debug(f"'--dev' argument provided by user.")
+
     requirements_txt: PosixPath = (
-        Path(Path(virtual_env).parent / Path(REQUIREMENTS_TXT)) if virtual_env else Path(REQUIREMENTS_TXT)
+        Path(Path(virtual_env).parent / Path(requirements_file)) if virtual_env else Path(requirements_file)
     )
 
-    if not requirements_txt.exists():
-        requirements_txt.touch()
+    log.info(f"Targeting {str(requirements_txt)} following 'pip' execution.")
+
+    requirements_txt.parent.mkdir(exist_ok=True)
+    requirements_txt.touch(exist_ok=True)
 
     user_provided_packages: List[PinnedPackage] = utils.extract_user_provided_packages(sys.argv[1:])
+
+    log.debug(
+        f"Extracted packages {[str(pkg) for pkg in user_provided_packages]} from user arguments. Running 'pip' command."
+    )
 
     utils.run_user_pip_cmd(sys.argv[1:])
 
